ad8d2c36be4dbfd2281e9814cc18e6e30dd9ea33
DevAvocado <kiril.bklv@gmail.com>
2024-02-07 17:20:39 +0100

    initial

---
+++ Models/LibraryDatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.Models;
+
+public class LibraryDatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+
+    public string DatabaseName { get; set; } = null!;
+
+    public string BooksCollectionName { get; set; } = null!;
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+using Library.Models;
+using Library.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.Configure<LibraryDatabaseSettings>(
+builder.Configuration.GetSection("LibraryDatabase"));
+
+builder.Services.AddSingleton<BooksService>();
+
+builder.Services.AddControllers()
+    .AddJsonOptions(
+        options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
